a018ca82b312c6339b33e3b7dde7701b25f0502b
JulianCallejas <juliecallejas@hotmail.com>
2024-11-28 23:47:41 -0500

    deploy test

--- back/rustik/src/main/java/com/rustik/rustik/RustikApplication.java
+++ back/rustik/src/main/java/com/rustik/rustik/RustikApplication.java
@@ -4,7 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.rustik.rustik"})
 public class RustikApplication {
 
 	public static void main(String[] args) {
